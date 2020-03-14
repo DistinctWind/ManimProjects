8a71319ca7b91ac78b9ecbcbfb11fb7a4781f603
Gorun <zq_zhouqi@qq.com>
2020-03-14 20:57:38 +0800

    添加BeginScene

--- Homework#1.py
+++ Homework#1.py
@@ -2,3 +2,12 @@ from manimlib.imports import *
 import numpy
 import math
 
+class BeginScene(Scene) :
+    def construct(self) :
+        title = Text("Manim Homework Vol.1", font='calibri')
+        author = Text("Made by @DistinctWind", font='calibri').scale(0.5)
+        title.move_to(UP*0.5)
+        author.move_to(DOWN*0.5)
+        self.play(Write(title))
+        self.play(Write(author))
+        self.wait(2)
