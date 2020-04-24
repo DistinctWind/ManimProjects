db16627ba8eeb84e5459d270869558fdf96ba1eb
DistinctWind <zq_zhouqi@qq.com>
2020-04-24 19:18:23 +0800

    add hard_maze

--- ocular_algorithm/0x02_BasicDeepthFirstSearch.py
+++ ocular_algorithm/0x02_BasicDeepthFirstSearch.py
@@ -16,4 +16,21 @@ class Begin_introduction(Scene) :
         self.add(easy_maze)
         self.wait(2)
 
-        
+        hard_maze = Maze(9, 16, scale_factor=0.75)
+        hard_maze.set_start(1, 1)
+        hard_maze.set_end(9, 16)
+        hard_maze.set_bar_by_str(
+            """
+            0001101101001010
+            0100000001101000
+            0101110100001011
+            0100010010100000
+            0111101010101111
+            0010001110001000
+            1000100010111010
+            0010101011100010
+            1110101000001010
+            """
+        )
+        self.play(ReplacementTransform(easy_maze, hard_maze))
+        self.wait(2)
